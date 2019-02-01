a78a0613eb84bbe24ceb865e7d2c18d65aae387f
Afshin Moazami <afshing@users.noreply.github.com>
2019-01-31 21:33:58 -0500

    link css and html + scope

--- w3/js/script.js
+++ w3/js/script.js
@@ -1,29 +1,44 @@
 'use strict'
 function isPalindromic2(userInput) {
   return (userInput
+    .toLowerCase()
     .split('')
     .reverse()
-    .join('') === userInput);
+    .join('') === userInput.toLowerCase());
 }
+let min = 'baz bar';
+function generateRandom (min = 0, max= 1000) {
+  let testScope= 'foo bar';
+  console.log('inside', min);
+
+  let anotherFunction = function() {
+    console.log('inside anotherFunction');
+  }
+  console.log(typeof anotherFunction);
+}
+generateRandom();
+
+// console.log('outside', testScope);
 
 function isPalindromic(userInput) {
-  let userInputArray = userInput.split('');
+  let spaceFree = userInput.split(' ').join('');
+  let userInputArray = spaceFree.toLowerCase().split('');
   userInputArray.reverse();
   let reverseInput = userInputArray.join('');
-
-return (userInput === reverseInput);
-  // if (userInput === reverseInput) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
-
+  return (spaceFree.toLowerCase() === reverseInput);
 }
-// let name = 'john';
-// let result = name.split('');
-// console.log(result);
 
-console.log(isPalindromic('mom'));
+function submitForm() {
+  document
+  .querySelector('.hide')
+  .style = 'display: block';
+
+
+
+  alert(isPalindromic(text));
+}
+
+// console.log(isPalindromic2('Mom'));
 // console.log(isPalindromic('concordia'));
 //
 // let test = 'Mon';
