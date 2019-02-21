eda6b66c351b54fff27aa971cff835efd60441d6
Afshin Moazami <afshing@users.noreply.github.com>
2019-02-21 17:33:25 -0500

    w6 initial commit

---
+++ w6/js/app.js
@@ -0,0 +1,33 @@
+'use strict'
+function printTitles() {
+  // your code goes here
+  console.log('SOMETHING');
+}
+
+function printAuthors() {
+    // your code goes here
+    console.log('SOMETHING ELSE');
+}
+
+function findShortest() {
+    // your code goes here
+    return 'TITLE OF THE BOOK';
+}
+
+function aboveThree() {
+  let result = [];
+  console.log(books[0]);
+
+  return result;
+}
+
+aboveThree();
+
+function findAuthors () {
+  let result = {};
+  /* {
+    "Females and Harry Potter" : ["Ruthann Mayes-Elma"],
+    "Harry Potter and International Relations":  ["Daniel H. Nexon", "Iver B. Neumann"],
+  }*/
+  return result;
+}
