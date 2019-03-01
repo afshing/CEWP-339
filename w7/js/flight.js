f6eae9594d82655b5cfb9647aa24c720b601e07d
Afshin Moazami <afshing@users.noreply.github.com>
2019-02-28 21:19:34 -0500

    date stuff

--- w7/js/flight.js
+++ w7/js/flight.js
@@ -22,6 +22,14 @@ function Flight(airline,
   this.priceInCAD = function (rate) {
     return this.priceInUSD * rate;
   }
+
+  this.duration = () => {
+    return (this.arrTime - this.depTime)/(1000 * 60 * 60);
+  }
+
+  this.numberOfOvernights = function() {
+    return (this.arrTime.getDate() - this.depTime.getDate());
+  }
 }
 //console.log(myFancyFlight.numberOfStops());
 
@@ -29,8 +37,8 @@ function Flight(airline,
 let torAirport = new Airport('Peterson', 'Toronto', 'YYZ');
 
 let myFancyFlight = new Flight('Air Canada',
-        new Date(),
-        new Date(),
+        new Date("2019-03-17T22:24:00"),
+        new Date("2019-03-17T03:24:00"),
         [torAirport],
         8500.58,
         mtlAirport,
@@ -43,3 +51,6 @@ console.log(myFancyFlight.stops[0].name);
 console.log(myFancyFlight.arrTime);
 console.log(myFancyFlight.numberOfStops());
 console.log(myFancyFlight.priceInCAD(1.3));
+console.log(myFancyFlight.arrTime.getHours());
+console.log(myFancyFlight.duration());
+console.log(myFancyFlight.numberOfOvernights());
