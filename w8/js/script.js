8a50b490a3f013e29435264e8b4aeb43f94cd1b2
Afshin Moazami <afshing@users.noreply.github.com>
2019-03-07 20:38:25 -0500

    pseudo code for beer

--- w8/js/script.js
+++ w8/js/script.js
@@ -1,3 +1,21 @@
+function findBeer() {
+  // 1. get the city name from input
+  // 2. create a url
+  // https://api.openbrewerydb.org/breweries?by_city=Washington
+  // 3. send a request
+  // 4. console.log the responseText
+
+
+}
+
+
+
+
+
+
+
+
+
 function reqListener () {
   console.log(this.responseText);
 }
