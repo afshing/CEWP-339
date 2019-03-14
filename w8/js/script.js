3041099f2d3cd8a4029c23c27ac81d60ff4d4ab4
Afshin Moazami <afshing@users.noreply.github.com>
2019-03-14 19:27:11 -0400

    beer exercise

--- w8/js/script.js
+++ w8/js/script.js
@@ -1,35 +1,71 @@
 function findBeer() {
-  // 1. get the city name from input
-  // 2. create a url
-  // https://api.openbrewerydb.org/breweries?by_city=Washington
-  // 3. send a request
-  // 4. console.log the responseText
+  let city = getCityName();
+  let name = getName();
 
+  let urlStr = createURL(city, name);
+  sendRequest(urlStr);
+}
 
+function getCityName() {
+  let city = document.querySelector('#city').value;
+  return city;
+}
+
+function getName() {
+  let name = document.querySelector('#name').value;
+  return name;
 }
 
 
-
-
-
-
-
-
+function createURL(city, name) {
+  return `https://api.openbrewerydb.org/breweries?by_city=${city}&by_name=${name}`;
+}
 
 function reqListener () {
-  console.log(this.responseText);
+  let list = JSON.parse(this.responseText);
+  showResult(list);
 }
 
-function submit() {
-  var req = new XMLHttpRequest();
-  //req.addEventListener("load", reqListener);
-
-  req.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-      console.log(JSON.parse(this.responseText));
-    }
-  };
-
-  req.open("GET", "https://www.googleapis.com/books/v1/volumes?q=harry");
-  req.send();
+function showResult(list) {
+  let result = '';
+  list.forEach((item) => {
+    result += `<li>${item.name}</li>`
+    // result += '<li>' + item.name + '</li>';
+  });
+  console.log(result);
+  document.getElementById('beers').innerHTML = result;
 }
+
+function sendRequest(urlStr) {
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", urlStr);
+  oReq.send();
+
+}
+
+
+
+
+
+
+
+
+//
+// function reqListener () {
+//   console.log(this.responseText);
+// }
+//
+// function submit() {
+//   var req = new XMLHttpRequest();
+//   //req.addEventListener("load", reqListener);
+//
+//   req.onreadystatechange = function() {
+//     if (this.readyState === 4 && this.status === 200) {
+//       console.log(JSON.parse(this.responseText));
+//     }
+//   };
+//
+//   req.open("GET", "https://www.googleapis.com/books/v1/volumes?q=harry");
+//   req.send();
+// }
